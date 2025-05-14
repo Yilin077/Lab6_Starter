846b5c20b46780219ce1bca439c0a5806948df5b
Yilin077 <83322578+Yilin077@users.noreply.github.com>
2025-05-14 04:09:58 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -21,6 +21,9 @@ function init() {
  * @returns {Array<Object>} An array of recipes found in localStorage
  */
 function getRecipesFromStorage() {
+  return JSON.parse(localStorage.getItem('recipes')) || [];
+}
+
 	// A9. TODO - Complete the functionality as described in this function
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
@@ -34,6 +37,7 @@ function getRecipesFromStorage() {
  * @param {Array<Object>} recipes An array of recipes
  */
 function addRecipesToDocument(recipes) {
+
 	// A10. TODO - Get a reference to the <main> element
 	// A11. TODO - Loop through each of the recipes in the passed in array,
 	//            create a <recipe-card> element for each one, and populate
